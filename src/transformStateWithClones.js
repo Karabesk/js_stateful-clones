c7b070c0191f417c8e9560dab4817ef46f89d20e
Karina Turchyna <kari.tu99@gmail.com>
2025-02-24 12:39:42 +0200

    implement task

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,39 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const statesAfterActions = [];
+  const stateCopy = { ...state };
+
+  for (const action of actions) {
+    const { type, extraData, keysToRemove } = action;
+
+    switch (type) {
+      case 'clear':
+        for (const key in stateCopy) {
+          delete stateCopy[key];
+        }
+        break;
+
+      case 'addProperties':
+        for (const key in extraData) {
+          stateCopy[key] = extraData[key];
+        }
+        break;
+
+      case 'removeProperties':
+        for (const key in keysToRemove) {
+          delete stateCopy[keysToRemove[key]];
+        }
+        break;
+
+      default:
+        throw new Error(`Unknown action type: ${type}`);
+    }
+
+    statesAfterActions.push(Object.assign({}, stateCopy));
+  }
+
+  return statesAfterActions;
 }
 
 module.exports = transformStateWithClones;
