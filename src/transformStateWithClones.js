2d3b8041f3b7ff61ad331cb9e851df3bb929e947
Karina Turchyna <kari.tu99@gmail.com>
2025-02-24 12:50:18 +0200

    address issues

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -8,27 +8,27 @@
  */
 function transformStateWithClones(state, actions) {
   const statesAfterActions = [];
-  const stateCopy = { ...state };
+  let stateCopy = { ...state };
 
   for (const action of actions) {
     const { type, extraData, keysToRemove } = action;
 
     switch (type) {
       case 'clear':
-        for (const key in stateCopy) {
-          delete stateCopy[key];
-        }
+        stateCopy = {};
         break;
 
       case 'addProperties':
         for (const key in extraData) {
+          stateCopy = { ...stateCopy };
           stateCopy[key] = extraData[key];
         }
         break;
 
       case 'removeProperties':
-        for (const key in keysToRemove) {
-          delete stateCopy[keysToRemove[key]];
+        for (const key of keysToRemove) {
+          stateCopy = { ...stateCopy };
+          delete stateCopy[key];
         }
         break;
 
